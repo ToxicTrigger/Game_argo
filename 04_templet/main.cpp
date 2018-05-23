891e98887995c485896f333e9542758fa4f75789
MrMiner <syoa1996@gmail.com>
2018-05-24 04:42:18 +0900

    ㅇㅇ

--- 04_templet/main.cpp
+++ 04_templet/main.cpp
@@ -1,34 +1,20 @@
 #include <iostream>
-#include "CkQ.h"
 #include <string>
 #include <chrono>
+#include <queue>
+#include <deque>
 using namespace std;
 
 void main()
 {
-	CkQ<int> q_int(10);
-	CkQ<float> q_float(10);
-	CkQ<string> q_string(10);
+	deque<int> dq;
+	deque<int>::iterator iter;
+	dq.push_back(22);
+	dq.push_back(11);
 
-	auto s = std::chrono::high_resolution_clock::now();
-	for (int i = 0; i < q_int.get_max_size(); i++)
+	for (iter = dq.begin(); iter != dq.end(); ++iter)
 	{
-		q_int.enQ(i);
-		q_float.enQ(sin(i));
-		q_string.enQ(std::to_string(i + 2));
+		cout << *iter << endl;
 	}
-	auto f = std::chrono::high_resolution_clock::now();
-	cout << "input : " << std::chrono::duration_cast<std::chrono::nanoseconds>(f - s).count() << "ns\n";
-
-	s = std::chrono::high_resolution_clock::now();
-	while (!q_int.isEmpty())
-	{
-		printf("int : %d\n", q_int.deQ());
-		printf("float : %f\n", q_float.deQ());
-		printf("str : %s\n", q_string.deQ().c_str());
-	}
-	f = std::chrono::high_resolution_clock::now();
-	cout << "output : " << std::chrono::duration_cast<std::chrono::nanoseconds>(f - s).count() << "ns\n";
-
 	getchar();
 }
